fe10626a06c7578f9d8828faa77c6ef7a406556d
Adolfo Alvarez <adolfo.alvar3z.p@gmail.com>
2023-04-25 22:25:29 -0400

    Diseño finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//toast
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', () => {
+        const toast = new bootstrap.Toast(toastLiveExample)
+
+        toast.show()
+    })
+}
+//tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
